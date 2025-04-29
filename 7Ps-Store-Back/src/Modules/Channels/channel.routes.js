978845de24e163f97ea6fc9206e4fafa97a4b37d
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-04-29 04:31:23 +0300

    handle auth middlewares, error page, router outlet and navigation up to top of page

--- 7Ps-Store-Back/src/Modules/Channels/channel.routes.js
+++ 7Ps-Store-Back/src/Modules/Channels/channel.routes.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const channelController = require('../Channels/channel.controler');
-
+const authMiddleware = require('../../middlewares/authMiddleware');
+const adminFilter = require('../../middlewares/adminMiddleware');
+const { createChannelSchema, updateChannelSchema } = require('../Channels/channel.validator');
+const validate = require('../../middlewares/errorHandler');
 // Create a new channel
-router.post('/', channelController.createChannel);
+router.post('/', authMiddleware, adminFilter, validate(createChannelSchema), channelController.createChannel);
 
 // Get all channels
-router.get('/', channelController.searchChannels);
+router.get('/', authMiddleware, channelController.searchChannels);
 
 // Get Arab countries for filtering
 router.get('/arab-countries', channelController.getArabCountries);
 
 // Import multiple channels
-router.post('/import', channelController.importChannels);
+router.post('/import', authMiddleware, adminFilter, channelController.importChannels);
 
 // Get a single channel by ID
-router.get('/:id', channelController.getChannelById);
+router.get('/:id', authMiddleware, channelController.getChannelById);
 
 // Update a channel
-router.put('/:id', channelController.updateChannel);
+router.put('/:id', authMiddleware, adminFilter, validate(updateChannelSchema), channelController.updateChannel);
 
 // Delete a channel
-router.delete('/:id', channelController.deleteChannel);
+router.delete('/:id', authMiddleware, adminFilter, channelController.deleteChannel);
 
 module.exports = router;
--- 7Ps-Store-Front/src/app/app.component.ts
+++ 7Ps-Store-Front/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { ChannelsComponent } from "./pages/channels/channels.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = '7Ps-Store-Front';
+  constructor(private router: Router) { }
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login' || this.router.url === '/error';
+  }
 }
