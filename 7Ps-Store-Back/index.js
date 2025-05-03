9499ce719f8aeb3ea71e58d08926297c9e41b323
mohamedAbdelwahabali5 <mohamedabdelwahabali5@gmail.com>
2025-05-03 22:26:59 +0300

    Update index.js

--- 7Ps-Store-Back/index.js
+++ 7Ps-Store-Back/index.js
@@ -16,8 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: ['http://localhost:4200', 'https://7ps-store.netlify.app', process.env.FRONTEND_URL],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Access-Control-Allow-Headers', 'Content-Type', 'Authorization']
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 // Database connection
