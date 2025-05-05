4f085827c651ba5e20cece7a86a0801e2b816f6f
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-05-05 03:55:49 +0300

    handle some styles and logic of control panel111

--- 7Ps-Store-Front/src/environments/environment.prod.ts
+++ 7Ps-Store-Front/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://7ps-store.up.railway.app',
+  // apiUrl: 'https://7ps-store-production-83c2.up.railway.app',
+  // apiUrl: 'http://localhost:4800',
+
 };
