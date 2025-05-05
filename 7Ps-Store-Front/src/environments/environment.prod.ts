e544be651918aa8da0aeba0e897947bd62d03568
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-05-05 07:06:37 +0300

    handle some issues

--- 7Ps-Store-Front/src/environments/environment.prod.ts
+++ 7Ps-Store-Front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://7ps-store.up.railway.app',
+  apiUrl: 'https://7ps-store.up.railway.app',
   // apiUrl: 'https://7ps-store-production-83c2.up.railway.app',
-  apiUrl: 'http://localhost:4800',
+  // apiUrl: 'http://localhost:4800',
 };
